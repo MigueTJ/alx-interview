ebd63db1efe1431e188a469c0d3edb9e70a92971
Migue Tj <miguejustus@gmail.com>
2024-05-02 13:21:41 +0300

    Update to 0-making_change.py - makes the file executable

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,18 +6,17 @@ needed to meet a given amount total using a greedy programming approach.
 
 
 def makeChange(coin, total):
-	"""
-	Determines the fewest number of coins needed to meet a given amount total.
-	"""
-	if total <= 0:
-		return 0
-	coins.sort(reverse=True)
-	coin_count = 0
-	for coin in coins:
-		if total >= coin:
-			coin_count += total // coin
-			total %= coin
-		if total == 0:
-			return coin_count
-	return -1 if total != 0 else coint_count
-	
+    """
+    Determines the fewest number of coins needed to meet a given amount total.
+    """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    coin_count = 0
+    for coin in coins:
+        if total >= coin:
+            coin_count += total // coin
+            total %= coin
+        if total == 0:
+            return coin_count
+    return -1 if total != 0 else coint_count
