441d12465174727d4ebe390270647e00e8de878d
Migue Tj <miguejustus@gmail.com>
2024-05-02 12:59:23 +0300

    Greedy Algorithm quiz

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module contains a function that calculates the fewest number of coins
+needed to meet a given amount total using a greedy programming approach.
+"""
+
+
+def makeChange(coin, total):
+	"""
+	Determines the fewest number of coins needed to meet a given amount total.
+	"""
+	if total <= 0:
+		return 0
+	coins.sort(reverse=True)
+	coin_count = 0
+	for coin in coins:
+		if total >= coin:
+			coin_count += total // coin
+			total %= coin
+		if total == 0:
+			return coin_count
+	return -1 if total != 0 else coint_count
+	
---
+++ README.md
@@ -0,0 +1,1 @@
+# Title: alx-interview
